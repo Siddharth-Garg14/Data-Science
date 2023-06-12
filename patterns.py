ca8f4e3bd01502a3879fa78eb37c67d5ea98e44a
SIDDHARTH GARG <90087986+Siddharth-Garg14@users.noreply.github.com>
2023-06-12 23:41:14 +0530

    Practice some questions on patterns in python

---
+++ patterns.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ****
+# ****
+# ****
+# ****
+
+# In[6]:
+
+
+n=int(input())
+i=1
+while (i<=n):
+    j=1
+    while (j<=n):
+        print("*",end='')
+        j=j+1
+    print()
+    i=i+1
+
+
+# 1111
+# 2222
+# 3333
+# 4444
+
+# In[7]:
+
+
+n=int(input())
+i=1
+while (i<=n):
+    j=1
+    while(j<=n):
+        print(i,end='')
+        j=j+1
+    print()
+    i=i+1
+
+
+# In[ ]:
+
+
+
+
